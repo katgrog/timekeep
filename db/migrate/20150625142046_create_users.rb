236ef963d87cc3841615db97bddefdafca75f516
Katie Grogan <katgrog@hotmail.com>
2015-06-25 15:23:18 +0100

    add form for user

---
+++ db/migrate/20150625142046_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+
+      t.string :first_name
+      t.string :last_name
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
+      t.string :username 
+      	
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625132630) do
+ActiveRecord::Schema.define(version: 20150625142046) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.boolean  "admin",           default: false
+    t.string   "username"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "watches", force: :cascade do |t|
     t.string   "name"

07f5708f24c0a5c293260bd1134319c6e6f684ce
Katie Grogan <katgrog@hotmail.com>
2015-06-25 15:16:31 +0100

    route to users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/edit'
-
   root "watches#index"
   resources :watches 
+  resources :users, except: :index 
 end
